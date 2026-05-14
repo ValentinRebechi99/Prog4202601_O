2f3b4ea90bebf7ec7914c660f6d8148d9a49586e
ValentinRebechi99 <71794802+ValentinRebechi99@users.noreply.github.com>
2026-05-14 16:45:39 -0300

    Agregado de Carga html
    Se agregan los html de carga de datos, tambien se corrige diseño de tablas

--- api/prueba.js
+++ api/prueba.js
@@ -1,1 +1,4 @@
-console.log("Hola mundo desde la API de prueba");
+const response = await fetch('http://127.0.0.1:5500/web/js/cursos.json');
+const cursos = await response.json();
+
+console.log(cursos);
