5f5f39770c5d369b9b0bbe08c7d08005fb88f6d7
ValentinRebechi99 <71794802+ValentinRebechi99@users.noreply.github.com>
2026-05-15 16:55:07 -0300

    Corrección en tabla Estudiantes
    Se corrige tabla para que muestre correctamente

--- web/js/estudiantes.js
+++ web/js/estudiantes.js
@@ -3,17 +3,16 @@ const cargarEstudiantes = async () => {
         const respuesta = await fetch("js/estudiantes.json");
         const datos = await respuesta.json();
 
-        const tabla = document.getElementById("tbody");
+        const tabla = document.getElementById("tbodyEstudiantes");
         datos.forEach(estudiante => {
             const fila = document.createElement("tr");
             fila.innerHTML = `
-            <td>${estudiante.idEstudiante}</td>
+            <td>${estudiante.documento}</td>
             <td>${estudiante.nombre}</td>
-            <td>${estudiante.descripcion}</td>
-            <td class="text-center">${new Date(estudiante.fechaInicio).toLocaleDateString()}</td>
-            <td class="text-center">${estudiante.cantidadHoras}</td>
-            <td class="text-center">${estudiante.inscriptosMax}</td>
-            <td class="text-center">${estudiante.estado}</td>`;
+            <td>${estudiante.apellido}</td>
+            <td>${estudiante.email}</td>
+            <td class="text-center">${new Date(estudiante.fechaNacimiento).toLocaleDateString()}</td>
+            <td class="text-center">${estudiante.activo ? "Sí" : "No"}</td>`;
             tabla.appendChild(fila);
         });
         document.getElementById("error").style.display = "none";
