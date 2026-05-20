09a516c405e07374c2d3d0b999a1558fbe26eed1
Ian <iancarab@live.com.ar>
2026-05-20 18:37:25 -0300

    fix cursos tableloading

--- js/cursos.js
+++ js/cursos.js
@@ -1,27 +1,46 @@
 const cargarCursos = async () => {
+
     try {
-        const respuesta = await fetch("js/cursos.json");
+
+        const respuesta = await fetch("./js/cursos.json");
+
+        if (!respuesta.ok) {
+            throw new Error("No se pudo cargar el archivo JSON");
+        }
+
         const datos = await respuesta.json();
 
+        console.log(datos);
+
         const tabla = document.getElementById("tbody");
+
         datos.forEach(curso => {
+
             const fila = document.createElement("tr");
+
             fila.innerHTML = `
-            <td>${curso.idCurso}</td>
-            <td>${curso.nombre}</td>
-            <td>${curso.descripcion}</td>
-            <td class="text-center">${new Date(curso.fechaInicio).toLocaleDateString()}</td>
-            <td class="text-center">${curso.cantidadHoras}</td>
-            <td class="text-center">${curso.inscriptosMax}</td>
-            <td class="text-center">${curso.estado}</td>`;
+                <td>${curso.idCurso}</td>
+                <td>${curso.nombre}</td>
+                <td>${curso.descripcion}</td>
+                <td>${new Date(curso.fechaInicio).toLocaleDateString()}</td>
+                <td>${curso.cantidadHoras}</td>
+                <td>${curso.inscriptosMax}</td>
+                <td>${curso.estado}</td>
+            `;
+
             tabla.appendChild(fila);
+
         });
-        document.getElementById("error").style.display = "none";
 
     } catch (error) {
-        document.getElementById("error").innerHTML = "<p>Error al cargar los cursos.</p>";
-        document.getElementById("error").style.display = "block";
+
+        console.error(error);
+
+        document.getElementById("error").innerHTML =
+            "Error al cargar los cursos";
+
     }
-}
+
+};
 
 document.addEventListener("DOMContentLoaded", cargarCursos);
