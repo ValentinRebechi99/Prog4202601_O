691aa9027e8f686c272698aa5a22c41a64457bde
Matias Serantes <matiasfs99@protonmail.com>
2026-06-14 17:46:44 -0300

    conexion del validador con el front

--- validators/cursos/cursosCreate.validation.js
+++ validators/cursos/cursosCreate.validation.js
@@ -9,6 +9,7 @@ const cursosCreateValidation = [
     (req, res, next) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
+            console.log(JSON.stringify(errors.array()))
             return res.status(400).json({ errors: errors.array() });
         }
         next();
